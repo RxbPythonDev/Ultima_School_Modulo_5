d4205fb6c735f386736b37ca7d5c8179beee3aef
RxbPythonDev <rxbpythondev@gmail.com>
2023-07-17 22:00:04 -0300

    Códigos praticados seguindo o conteúdo da plataforma na terceira semana do módulo 5

---
+++ Semana 3/base/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=50)
+    email = models.EmailField(max_length=75)
+    mensagem = models.TextField()
+    data = models.DateTimeField(auto_now_add=True)
+    
