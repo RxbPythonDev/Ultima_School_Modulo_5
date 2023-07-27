f9c4ff1b786daf150261218cbb4f4844aec48e25
RxbPythonDev <rxbpythondev@gmail.com>
2023-07-27 18:14:26 -0300

    Atividade da quarta semana do módulo 5

---
+++ Semana 4/Atividade M5S4/ultima/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from base.views import inicio, contato
+
+urlpatterns = [
+    path('', inicio),
+    path('contato/', contato),
+    path('reserva/', include('reserva.urls', namespace='reserva')),
+    path('admin/', admin.site.urls),
+]
