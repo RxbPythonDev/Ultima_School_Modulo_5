cc97d87a3378118eb419ac67a23aa47317d42f18
RxbPythonDev <rxbpythondev@gmail.com>
2023-07-27 17:32:53 -0300

    Códigos praticados na aula sobre Administrador do Django e Sistema de Usuários

---
+++ Semana 4/Aula gravada/ultima_aula_gravada/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+
+from base.views import inicio, inscrever
+
+from eventos.views import evento
+
+
+urlpatterns = [
+    path('', inicio, name='inicio'),
+    path('inscreva-se/', inscrever, name='inscrever'),
+    path('eventos/<int:id>/', evento, name='evento'),
+    path('admin/', admin.site.urls),
+]
